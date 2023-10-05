e150cc00e3ac0c296437e60a247466031a980060
Maria <flamik@inbox.ru>
2023-10-05 13:24:37 +0300

    Add hw4

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int Prompt (string message)
+{
+    Console.Write (message);
+    string readInput = Console.ReadLine();
+    int result = int.Parse (readInput);
+    return result;
+}
+int SumAllDigit (int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number %  10;
+        number = number / 10;
+    }
+    return result;
+}
+int number = Prompt("Введите число: ");
+Console.WriteLine($"Сумма всех чисел в цифре {number} = {SumAllDigit(number)}");
